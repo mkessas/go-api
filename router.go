7d493886fab65df3c8389e81bc9e16f63638ef15
Mourad Kessas <mourad.kessas@spark.co.nz>
2017-12-14 15:56:44 +1300

    Added http authentication

--- router.go
+++ router.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"strconv"
 
+	"github.com/goji/httpauth"
 	"github.com/gorilla/mux"
 )
 
@@ -23,13 +24,16 @@ func router() {
 
 		fmt.Printf("Adding handler for '%s'\n", p.path)
 		//http.HandleFunc(p.path, p.handler)
-		r.HandleFunc(p.path, p.handler).Methods(p.method)
+		if p.protected {
+			r.Handle(p.path, httpauth.SimpleBasicAuth("admin", "admin")(http.HandlerFunc(p.handler))).Methods(p.method)
+		} else {
+			r.HandleFunc(p.path, p.handler).Methods(p.method)
+		}
 	}
 
 	r.PathPrefix("/api").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "File Not Found")
 	})
-
 	r.Handle("/", http.FileServer(http.Dir("static")))
 	http.ListenAndServe(":"+strconv.Itoa(port), r)
 }
