088a98caf7c98f4f21a62a85da953cd799410183
Mourad Kessas <mourad.kessas@spark.co.nz>
2017-12-14 15:59:05 +1300

    Moved route definitions routes.go

---
+++ routes.go
@@ -0,0 +1,6 @@
+package main
+
+var paths = []path{
+	{path: "/api/test1", handler: test1Handler, method: "GET", protected: true},
+	{path: "/api/test2", handler: test2Handler, method: "GET", protected: false},
+}
--- types.go
+++ types.go
@@ -8,8 +8,3 @@ type path struct {
 	method    string
 	protected bool
 }
-
-var paths = []path{
-	{path: "/api/test1", handler: test1Handler, method: "GET", protected: true},
-	{path: "/api/test2", handler: test2Handler, method: "GET", protected: false},
-}
